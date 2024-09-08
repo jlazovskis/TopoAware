58d038a485d6a07b13803e620953eb82e9617615
jlazovskis <jlazovskis@gmail.com>
2024-09-08 14:41:50 +0300

    read file as point cloud in ripser

--- hvt.cpp
+++ hvt.cpp
@@ -4,12 +4,49 @@
 
 // stl
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 // hvt
+namespace hvt {
+    typedef int64_t index;
+}
+
 #include <simplicial_complex.h>
 
+static const size_t num_coefficient_bits = 8;
 
-int main () {
 
-	return 0;
+int main (int argc, char** argv) {
+	const char* filename = nullptr;
+	float threshold;
+
+	// Read arguments
+	for (int i = 1; i < argc; ++i) {
+		const std::string arg(argv[i]);
+
+		// Threshold
+		if (arg == "--threshold") {
+			std::string parameter = std::string(argv[++i]);
+			size_t next_pos;
+			threshold = std::stof(parameter, &next_pos);
+
+		// Main file
+		} else {
+			filename = argv[i];
+		}
+	}
+
+
+	// Initialize distance matrix class
+	hvt::simplicial_complex cpx;
+
+	// Read input file
+	cpx.load_point_cloud(filename, threshold);
+
+	// Testing
+	cpx.print_me();
+
+	return 1;
 }
--- include/simplicial_complex.h
+++ include/simplicial_complex.h
@@ -1,10 +1,64 @@
 namespace hvt {
 	
 	class simplicial_complex {
+		private:
+			// The list of points
+			std::vector<std::vector<float>> points;
+
+			// A list of neighbors for each point
+			std::vector<std::vector<hvt::index>> neighbors;
+			
+			// The number of edges
+			hvt::index num_edges;
 
 	    public:
-
+	    	// Constructor
 	    	simplicial_complex() {};
 
-	}
+	    	// Load point cloud
+	    	// Taken from https://github.com/Ripser
+	    	bool load_point_cloud( const std::string filename, const float threshold ) {
+				
+				// Check to make sure file can be read
+    			std::ifstream dummy( filename.c_str() );
+    			if( dummy.fail() ) {
+					std::cerr << "couldn't open file " << filename << std::endl;
+					return false;
+    			}
+
+    			// Read in the data
+                this->points = points;
+				std::string line;
+                std::ifstream input_stream( filename.c_str() );
+
+				float value;
+				while (std::getline(input_stream, line)) {
+					std::vector<float> point;
+					std::istringstream s(line);
+					while (s >> value) {
+						point.push_back(value);
+						s.ignore();
+					}
+					if (!point.empty()) points.push_back(point);
+					//assert(point.size() == points.front().size());
+				}
+			
+    			return true;
+
+	    	};
+
+
+            // Print for testing
+            void print_me() {
+                this->points = points;
+                for ( int col = 0; col < points.size(); col++ ) {
+                    for (const float& value : points[col]) { 
+                        std::cout << value << ",";
+                    }
+                std::cout << "\n";
+                }
+            std::cout << std::endl;
+            };
+
+	};
 }
