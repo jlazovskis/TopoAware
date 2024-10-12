76b507b601b15db23f2dd322387632845f73096a
jlazovskis <jlazovskis@gmail.com>
2024-10-13 00:04:31 +0100

    r package all add

---
+++ R_package/hvt/src/hvt_sourceCpp.cpp
@@ -0,0 +1,89 @@
+// hypervolume-t 0.1.0 (port to R)
+// Created by Jānis Lazovskis 
+// GPL-3 license 
+
+#include <misc.h>
+#include <point_cloud.h>
+#include <split_points.h>
+#include <sparsify_points.h>
+
+#include <Rcpp.h>
+// [[Rcpp::export]]
+
+// Main runtime
+Rcpp::DataFrame hypervolume_t(
+		Rcpp::DataFrame data,       // The input data frame
+		float dist_barycenter,      // Distance below which all pairs and triples will be subdivided
+		float dist_sparsify)        // Minimum distance between all sparsified points
+	{
+
+	// TODO: Check arguments
+	std::cout << "hypervolume-t 0.1.0\n--------------------\n";
+
+	// Start timer
+	auto start = std::chrono::high_resolution_clock::now();
+
+	// Initialize point cloud class
+	hvt::point_cloud data_step0;
+
+	//R// Convert input from R to C++
+    const int dim = data.size();
+    const int samples = data.nrows();
+    for ( int i = 0; i < samples; i++ ) {
+    	hvt::point point;
+    	for ( int j = 0; j < dim; j++ ) {
+        	Rcpp::NumericVector column = data[j];
+    		point.push_back(column[i]);
+    	}
+    	data_step0.add_point(point);
+    } 
+	
+	// Find neighbors
+	data_step0.find_neighbors( dist_barycenter );
+
+	// Add barycenters
+	std::cout << "Adding barycenters to all pairs and triples within " << dist_barycenter << "... " << std::flush; 
+	hvt::point_cloud data_step1;
+	std::vector<int> points_added;
+	hvt::split_points( data_step0, data_step1, points_added, dist_barycenter );
+	std::cout << " done (" << data_step1.get_size() << " points = " << points_added[0] << " from pairs, " << points_added[1] << " from triples)\n";
+
+	// Sparsify
+	std::cout << "Sparsifying with minimum distance " << dist_sparsify << "... " << std::flush; 
+	hvt::point_cloud data_step2;
+	hvt::sparsify_points( data_step1, data_step2, dist_sparsify );
+	std::cout << " done (" << data_step2.get_size() << " points)\n"; 		
+
+
+	//R// Convert output from C++ to R
+	//R// Step 1: Create container to hold columns
+	std::vector< Rcpp::NumericVector > data_out_container;
+   	for ( int j = 0; j < dim; j++ ) {
+   		Rcpp::NumericVector column;
+   		data_out_container.push_back(column);
+   	}
+
+   	//R// Step 2: Go through all points and add values to columns
+    const int samples_out = data_step2.get_size();
+	for ( int i = 0; i < samples_out; i++ ) {
+		hvt::point current_point;
+		data_step2.get_point(i,current_point);
+   		for ( int j = 0; j < dim; j++ ) {
+   			data_out_container[j].push_back(current_point[j]);
+   		}
+   	}
+
+   	//R// Step 3: Make R data type and add columns
+	Rcpp::DataFrame data_out;
+   	for ( int j = 0; j < dim; j++ ) {
+   		data_out.push_back(data_out_container[j], "x"+std::to_string(j));
+   	}
+
+	// Exit
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast< std::chrono::seconds >(stop - start);
+	std::cout << "--------------------\nFinished in " << duration.count() << " seconds" << std::endl;
+
+   	//R// Return final dataframe
+	return data_out;
+}
---
+++ R_package/hvt/src/include/split_points.h
@@ -0,0 +1,83 @@
+// This file is part of hypervolume-t 
+// Header file for barycentric subdivision of a point cloud
+
+namespace hvt {
+
+	void split_points(
+			hvt::point_cloud initial_point_cloud,
+			hvt::point_cloud& target_point_cloud,
+			std::vector<int>& points_added, 
+			const hvt::value sparsification_dist ) {
+		std::vector<int> new_point_count;
+		
+		// Check that the input point cloud has points initialized
+		const int size = initial_point_cloud.get_size();
+		assert (size > 0);
+		
+		// Populate the points
+		int counter1 = 0;
+		int counter2 = 0;
+		for ( hvt::index i = 0; i < size; i++ ) {
+		
+			// Temporary containers
+			hvt::point current_point;
+			hvt::neighbors current_neighbors;
+			initial_point_cloud.get_point(i,current_point);
+			initial_point_cloud.get_neighbors(i,current_neighbors);
+		
+			// Add same point
+			hvt::point point;
+			for (const hvt::value& value : current_point) { 
+				point.push_back(value);
+			}
+			target_point_cloud.add_point(point);
+		
+			// Iterate through neighbors
+			for ( int j = 0; j < current_neighbors.size() ; j++ ) {
+				const hvt::index index_j = get_index(current_neighbors[j]);
+				// const float dist_ij = initial_point_cloud.get_dist(i,index_j);
+		
+				// Add midpoint of every two points
+				std::vector< hvt::point > neighboring_points;
+				hvt::point current_point_b;
+				initial_point_cloud.get_point(index_j,current_point_b);
+				neighboring_points.push_back(current_point_b);
+		
+				// Declare, compute, record new point
+				hvt::point new_point;
+				hvt::point_average( current_point, neighboring_points, new_point );
+				target_point_cloud.add_point(new_point);
+				counter1 += 1;
+		
+				// Iterate once again for triples
+				for ( int k = j+1 ; k < current_neighbors.size() ; k++ ) {
+					const hvt::index index_k = get_index(current_neighbors[k]);
+					const float dist_jk = initial_point_cloud.get_dist(index_j,index_k);
+		
+
+					// Add another neighboring point
+					hvt::point current_point_c;
+					initial_point_cloud.get_point(index_k,current_point_c);
+					neighboring_points.push_back(current_point_c);
+					
+					// Declare, compute, record new point
+					hvt::point new_point2;
+					hvt::point_average( current_point, neighboring_points, new_point2 );
+					target_point_cloud.add_point(new_point2);
+					counter2 += 1;
+					
+					// Drop last element two have two starting points for next triple
+					neighboring_points.pop_back();
+
+				}
+			}
+		}
+		
+		// Record progress and finish
+		new_point_count.push_back(counter1);
+		new_point_count.push_back(counter2);
+		points_added = new_point_count;
+
+	};
+
+}
