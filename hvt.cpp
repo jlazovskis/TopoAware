c141062b14636cec506bec46b17aff359f39cbfd
jlazovskis <jlazovskis@gmail.com>
2024-10-28 16:26:42 +0000

    aggregate distance

--- hvt.cpp
+++ hvt.cpp
@@ -12,6 +12,7 @@ int main (int argc, char** argv) {
 	const char* filename_in = nullptr;
 	const char* filename_out = nullptr;
 	const char* filename_bcs = nullptr;
+	hvt::value dist_aggregate = 0;
 	hvt::value dist_barycenter = 0;
 	hvt::value dist_sparsify = 0;
 
@@ -30,6 +31,13 @@ int main (int argc, char** argv) {
 		// Barycentric subdivison output file
 		} else if (arg == "--export_bcs") {
 			filename_bcs = argv[++i];
+
+		// Threshold for aggregating points
+		// Every point will be at least this far from other points
+		} else if (arg == "--adist") {
+			std::string parameter = std::string(argv[++i]);
+			size_t next_pos;
+			dist_aggregate = std::stof(parameter, &next_pos);
 
 		// Threshold for adding barycenters
 		// For every pair of points less than this distance, a new point will be added
@@ -58,6 +66,10 @@ int main (int argc, char** argv) {
 		std::cerr << "Missing output file" << std::endl;
 			return 0;
 	}
+	if ( dist_aggregate <= 0 ) {
+		std::cerr << "Distance for aggregating either unset or nonpositive" << std::endl;
+		return 0;
+	}
 	if ( dist_barycenter <= 0 ) {
 		std::cerr << "Distance for barycentric subdivison either unset or nonpositive" << std::endl;
 		return 0;
@@ -83,15 +95,21 @@ int main (int argc, char** argv) {
 	}
 	else { std::cout << "done (" << data_step0.get_size() << " points)\n";}
 
+	// Sparsify
+	std::cout << "Aggregating with minimum distance " << dist_aggregate << "... " << std::flush; 
+	hvt::point_cloud data_step1;
+	hvt::sparsify_points( data_step0, data_step1, dist_aggregate );
+	std::cout << " done (" << data_step1.get_size() << " points)\n"; 
+
 	// Find neighbors
-	data_step0.find_neighbors( dist_barycenter );
+	data_step1.find_neighbors( dist_barycenter );
 
 	// Add barycenters
 	std::cout << "Adding barycenters to all pairs and triples within " << dist_barycenter << "... " << std::flush; 
-	hvt::point_cloud data_step1;
+	hvt::point_cloud data_step2;
 	std::vector<int> points_added;
-	hvt::split_points( data_step0, data_step1, points_added, dist_barycenter );
-	std::cout << " done (" << data_step1.get_size() << " points = " << points_added[0] << " from pairs, " << points_added[1] << " from triples)\n";
+	hvt::split_points( data_step1, data_step2, points_added, dist_barycenter );
+	std::cout << " done (" << data_step2.get_size() << " points = " << points_added[0] << " from pairs, " << points_added[1] << " from triples)\n";
 
 	// Export barycenters
 	if ( filename_bcs != NULL ) {
@@ -102,13 +120,13 @@ int main (int argc, char** argv) {
 
 	// Sparsify
 	std::cout << "Sparsifying with minimum distance " << dist_sparsify << "... " << std::flush; 
-	hvt::point_cloud data_step2;
-	hvt::sparsify_points( data_step1, data_step2, dist_sparsify );
-	std::cout << " done (" << data_step2.get_size() << " points)\n"; 		
+	hvt::point_cloud data_step3;
+	hvt::sparsify_points( data_step2, data_step3, dist_sparsify );
+	std::cout << " done (" << data_step3.get_size() << " points)\n"; 		
 
 	// Export
 	std::cout << "Exporting sparsified point cloud to file... " << std::flush; 
-	data_step2.export_points( filename_out );
+	data_step3.export_points( filename_out );
 	std::cout << "done" << std::endl;
 
 	// Exit
