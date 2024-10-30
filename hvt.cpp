20c678cbbf4d932f57cd505afef9808245b32a06
jlazovskis <jlazovskis@gmail.com>
2024-10-30 11:27:11 +0000

    mod

--- hvt.cpp
+++ hvt.cpp
@@ -196,11 +196,13 @@ int main (int argc, char** argv) {
 	duration = std::chrono::duration_cast< std::chrono::seconds >(current - start);
 	std::cout << "(time: " << duration.count() << " seconds) Making complement... " << std::flush; 
 	hvt::point_cloud data_step4;
-	hvt::make_complement( data_step3, data_step4, dist_sparsify );
+	std::vector<int> points_dropped;
+	hvt::make_complement( data_step3, data_step4, 15*dist_sparsify, points_dropped );
 	const bool header_flag4 = false;
-	std::string filename_out_str(filename_out);
-	data_step4.export_points( "complement-"+filename_out_str, header_flag4 );
-	std::cout << " done (" << data_step4.get_size() << " points)\n"; 	
+	//std::string filename_out_str(filename_out);
+	//data_step4.export_points( "complement-"+filename_out_str, header_flag4 );
+	data_step4.export_points( "../complement-out.csv", header_flag4 );
+	std::cout << " done (" << points_dropped[0] << " points initially, " << points_dropped[1] << " after)\n"; 	
 
 	// Export
 	current = std::chrono::high_resolution_clock::now();
--- include/make_complement.h
+++ include/make_complement.h
@@ -6,7 +6,8 @@ namespace hvt {
 	void make_complement(
 			hvt::point_cloud initial_point_cloud,
 			hvt::point_cloud& target_point_cloud,
-			float step_size) {
+			float step_size,
+			std::vector<int>& points_dropped) {
 		std::vector<int> new_point_count;
 
 		// Info about initial point cloud
@@ -15,19 +16,19 @@ namespace hvt {
 
 		// Find range of all dimensions
 		std::vector< std::pair< hvt::value, hvt::value > > ranges;
-		for ( int d=0; d<=dim; d++ ) {
+		for ( int d=0; d<dim; d++ ) {
 			std::vector< hvt::value > slice;
 			initial_point_cloud.get_slice(d, slice);
 		    const auto [min, max] = std::minmax_element(begin(slice), end(slice));
 		    std::pair<  hvt::value, hvt::value > current_range;
-		    current_range.first = *min-step_size;
-		    current_range.second = *max+step_size;
+		    current_range.first = *min-2*step_size;
+		    current_range.second = *max+5*step_size;
 		    ranges.push_back(current_range);
 		}
 
 		// Get number of steps in each dimension
 		std::vector< int > steps_bydim;
-		for ( int d=0; d<=dim; d++ ) {
+		for ( int d=0; d<dim; d++ ) {
 			const auto dv = hvt::fdiv(abs(ranges[d].second - ranges[d].first), step_size);
 			steps_bydim.push_back(dv[0]);
 		}
@@ -35,6 +36,7 @@ namespace hvt {
 		for ( int q : steps_bydim ) {
 			total_points = total_points*q;
 		}
+		points_dropped.push_back(total_points);
 
 		// Create binary array for whether or not points should exist
 		std::vector< bool > point_tracker(total_points, true);
@@ -44,33 +46,49 @@ namespace hvt {
 			hvt::point current_point;
 			initial_point_cloud.get_point(i, current_point);
 			std::vector< int > current_coordinate;
-			for ( int d=0; d<=dim; d++ ) {
+			for ( int d=0; d<dim; d++ ) {
 				const auto dv = hvt::fdiv(abs(current_point[d] - ranges[d].first), step_size);
 				current_coordinate.push_back(dv[0]);
 			}
 			int current_location = 1;
-			for ( int d=0; d<=dim; d++ ) {
+			for ( int d=0; d<dim; d++ ) {
 				int current_summand = current_coordinate[d];
 				for ( int dd=0; dd<d; dd++ ) {
 					current_summand = current_summand*steps_bydim[dd];
 				}
 				current_location += current_summand;
 			}
-			point_tracker[current_location] = (false && point_tracker[current_location]);
+			// point_tracker[current_location] = (false && point_tracker[current_location]);
+
+			// FIX TO GENERALIZE: remove all around point
+			std::vector<int> current_locations;
+			current_locations.push_back(current_location);
+			current_locations.push_back(current_location+1);
+			current_locations.push_back(current_location+steps_bydim[0]);
+			current_locations.push_back(current_location+steps_bydim[0]+1);
+			for ( int i : current_locations ) {
+				if ( i < total_points ) {
+					point_tracker[i] = (false && point_tracker[i]);
+				}
+			}
 		}
 
 		// Initialize the points in the positions that have not been dropped
+		int count = 0;
 		for ( int i=0; i<total_points; i++ ) {
 			if ( point_tracker[i] ) {
 				hvt::point current_point;
 				int multiplier = 1;
-				for ( int d=0; d<=dim; d++ ) {
+				for ( int d=0; d<dim; d++ ) {
 					const auto coeff = std::div(i%(multiplier*steps_bydim[d]), multiplier);
 					current_point.push_back(ranges[d].first + coeff.quot*step_size);
 					multiplier = multiplier*steps_bydim[d];
 				}
 				target_point_cloud.add_point(current_point);
+				count += 1;
 			}
 		}
+		points_dropped.push_back(count);
+
 	};
 }
