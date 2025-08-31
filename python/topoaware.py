2f34e07afb30636c17c210c4cbbfdc1369a033c6
jlazovskis <jlazovskis@gmail.com>
2025-08-31 11:17:17 +0100

    barycenter python func

--- python/topoaware.py
+++ python/topoaware.py
@@ -3,3 +3,13 @@ import numpy as np
 import pandas as pd
 import gudhi as gd
 from matplotlib import pyplot as plt
+
+# functions
+
+# input: SimplexTree class, vertex coordinates (nrows=npoints, ncols=ndims)
+# output: collection of points, imput complex points + all barycenters of input complex simplices
+def barycenters(simplex_tree, points):
+	points_return = []
+	for simplex in simplex_tree.get_simplices():
+		points_return.append(np.mean([points[s] for s in simplex[0]],axis=0))
+	return np.array(points_return)
