7f090221318806f8d6f80a1bf7192c236192d5e0
jlazovskis <jlazovskis@gmail.com>
2025-09-23 06:36:14 +0300

    continue

--- c++/topoaware.h
+++ c++/topoaware.h
@@ -1,6 +1,26 @@
 // This file is a header file for TopoAware 
 // Author: Jānis Lazovskis, 2025
 
+// external libraries
+#include <gudhi/sparsify_point_set.h>
+
 namespace tpaw {
+
+	// types
+    typedef int64_t index;
+	typedef double value;
+	typedef std::vector< tpaw::value > point;
+	typedef std::vector< tpaw::point > point_cloud;
+
+	// function
+	tpaw::point_cloud barycenters(gudhi::simplex_tree simplex_tree, tpaw::point_cloud points){
+
+	}
+
+	// function
+	tpaw::point_cloud grid_points(tpaw::point_cloud points, tpaw::value grid_interval, tpaw::point grid_origin){
+		
+	}
+
 
 }
--- r/topoaware.r
+++ r/topoaware.r
@@ -6,3 +6,15 @@ devtools::install_github("LMJL-Alea/rgudhi")
 library(rgudhi)
 
 # Load data
+
+# Functions
+barycenters <- function(simplex_tree, points){
+
+}
+
+
+grid_points <- function(points, grid_interval, grid_origin){
+
+}
+
+
