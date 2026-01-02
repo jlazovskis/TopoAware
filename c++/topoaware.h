bd45ff190b4e189af7793ef9af33a218f5d6292a
jlazovskis <jlazovskis@gmail.com>
2026-01-02 09:09:39 +0200

    c++ functions

--- c++/topoaware.h
+++ c++/topoaware.h
@@ -9,18 +9,19 @@ namespace tpaw {
 	// types
     typedef int64_t index;
 	typedef double value;
-	typedef std::vector< tpaw::value > point;
-	typedef std::vector< tpaw::point > point_cloud;
+	typedef std::vector< value > point;
+	typedef std::vector< point > point_cloud;
 
-	// function
-	tpaw::point_cloud barycenters(gudhi::simplex_tree simplex_tree, tpaw::point_cloud points){
+	// functions
+	// tpaw::point_cloud barycenters(gudhi::simplex_tree simplex_tree, tpaw::point_cloud points){
+	point_cloud barycentric_subdivision(point_cloud points, value radius, index max_dim){}
 
-	}
+	point_cloud sparsification(point_cloud points, value min_dist){}
 
-	// function
-	tpaw::point_cloud grid_points(tpaw::point_cloud points, tpaw::value grid_interval, tpaw::point grid_origin){
-		
-	}
+	point_cloud gridification(point_cloud points, value grid_interval, point grid_origin){}
 
+	point_cloud complement(point_cloud points, value grid_interval, index buffer){}
+
+	point_cloud thickening(point_cloud points, value grid_interval){}
 
 }
